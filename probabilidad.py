c82a7c462f5a98e1334f4df9feafccd42df83e90
Christian Munoz <chgeovany.15@gmail.com>
2021-07-03 13:18:44 -0500

    Probabilidad de no obtener 1

--- probabilidad.py
+++ probabilidad.py
@@ -14,13 +14,13 @@ def main(numero_de_tiros,numero_de_intentos):
         secuencia_de_tiros = tirar_dado(numero_de_tiros)
         tiros.append(secuencia_de_tiros)
 
-    tiros_con_1 = 0
+    tiros_sin_1 = 0
     for tiro in tiros:
-        if 1 in tiro:
-            tiros_con_1 += 1
+        if 1 not in tiro:
+            tiros_sin_1 += 1
     
-    probabilidad_tiros_con_1 = tiros_con_1/numero_de_intentos
-    print(f'Probabilidad de obtener por lo menos un 1 en {numero_de_tiros} tiros = {probabilidad_tiros_con_1}')
+    probabilidad_tiros_sin_1 = tiros_sin_1/numero_de_intentos
+    print(f'Probabilidad de no obtener por lo menos un 1 en {numero_de_tiros} tiros = {probabilidad_tiros_sin_1}')
 
 if __name__ == '__main__':
     numero_de_tiros = int(input('Cuantos tiros del dado: '))
