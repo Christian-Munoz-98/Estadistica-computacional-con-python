c1258d55b6eb51792a596ef550050f07ed69561b
Christian Munoz <chgeovany.15@gmail.com>
2021-07-03 13:47:36 -0500

    Probabilidad con dos dados

--- probabilidad.py
+++ probabilidad.py
@@ -14,16 +14,22 @@ def main(numero_de_tiros,numero_de_intentos):
         secuencia_de_tiros = tirar_dado(numero_de_tiros)
         tiros.append(secuencia_de_tiros)
 
-    tiros_sin_1 = 0
+    tiros_con_1 = 0
     for tiro in tiros:
-        if 1 not in tiro:
-            tiros_sin_1 += 1
+        if 1 in tiro:
+            tiros_con_1 += 1
     
-    probabilidad_tiros_sin_1 = tiros_sin_1/numero_de_intentos
-    print(f'Probabilidad de no obtener por lo menos un 1 en {numero_de_tiros} tiros = {probabilidad_tiros_sin_1}')
+    probabilidad_tiros_con_1 = tiros_con_1/numero_de_intentos
+
+    return probabilidad_tiros_con_1
+    #print(f'Probabilidad de no obtener por lo menos un 1 en {numero_de_tiros} tiros = {probabilidad_tiros_sin_1}')
 
 if __name__ == '__main__':
     numero_de_tiros = int(input('Cuantos tiros del dado: '))
     numero_de_intentos = int(input('Cuantas veces correra la simulacion: '))
 
-    main(numero_de_tiros,numero_de_intentos)
+    dado_1 = main(numero_de_tiros,numero_de_intentos)
+    dado_2 = main(numero_de_tiros,numero_de_intentos)
+
+    probabilidad_en_dos_dados = dado_1 * dado_2
+    print(f'La probabilidad de obtener por lo menos un 1 en dos dados al hacer  {numero_de_tiros} tiros = {probabilidad_en_dos_dados}')
