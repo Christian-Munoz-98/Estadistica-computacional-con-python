5e866d46790ee3e4b9419bec42bb1217f27d47ce
Christian Munoz <chgeovany.15@gmail.com>
2021-07-07 13:03:10 -0500

    Probabilidad de obtener un par

--- barajas.py
+++ barajas.py
@@ -1,4 +1,5 @@
-import random
+import random, collections
+from typing import Counter
 
 PALOS = ['espada', 'corazon', 'rombo', 'trebol']
 VALORES = ['as', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jota', 'reina','rey']
@@ -17,10 +18,35 @@ def obtener_mano(barajas, tamano_mano):
     return mano
 
 def main(tamano_mano, intentos):
-    pass
+    barajas = crear_baraja()
+
+    manos = []
+
+    for _ in range(intentos):
+        mano = obtener_mano(barajas, tamano_mano)
+        manos.append(mano)
+    
+    pares = 0
+    for mano in manos:
+        valores = []
+        for carta in mano:
+            valores.append(carta[1])
+        
+        counter = dict(collections.Counter(valores))
+        for val in counter.values():
+            if val == 2:
+                pares += 1
+                break
+
+    probabilidad_par = pares / intentos
+
+    print(f'La probabilidad de obtener un par en una mano de {tamano_mano} barajas es {probabilidad_par}')
+        
+
 
 
 if __name__ == '__main__':
-    barajas = crear_baraja()
-    mano = obtener_mano(barajas, 5)
-    print(mano)
+    tamano_mano = int(input('De cuantas barajas sera la mano: '))
+    intentos = int(input('Cuantos intentos para calcular la probabilidad: '))
+
+    main(tamano_mano,intentos)
